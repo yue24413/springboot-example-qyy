9b9b0abe7d4f3ca0b5ba7c5dfd301445ae98623a
yue24413 <2312400876@qq.com>
2024-09-07 18:30:03 +0800

    jdbc

---
+++ jdbc-examples/src/main/java/com/example/jdbcexamples/dox/User.java
@@ -0,0 +1,27 @@
+package com.example.jdbcexamples.dox;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.ReadOnlyProperty;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @CreatedBy
+    private int id;
+    private String name;
+    @ReadOnlyProperty
+    private LocalDateTime createTime;
+    @ReadOnlyProperty
+    private LocalDateTime updateTime;
+}
